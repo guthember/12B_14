7b6c51a733b8bd472a5bdb61a6804b97c45acd90
Erik Gúth <guthember@gmail.com>
2017-02-23 11:20:03 +0100

    Kezdemény

---
+++ IP.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// -127      A
+// 128 - 191 B
+// 192 - 223 C
+namespace IpCimek
+{
+    class IP
+    {
+        private int elso;
+        private int masodik;
+        private int harmadik;
+        private int negyedik;
+
+        public IP(string cim)
+        {
+            string[] oktett = new string[4];
+            oktett = cim.Split('.');
+            elso = Convert.ToInt32(oktett[0]);
+            masodik = Convert.ToInt32(oktett[1]);
+            harmadik = Convert.ToInt32(oktett[2]);
+            negyedik = Convert.ToInt32(oktett[3]);
+        }
+
+        public string Osztaly() 
+        {
+            if (elso < 128)
+            {
+                return "A";
+            }
+            else if (elso < 192)
+            {
+                return "B";
+            }
+            else
+            {
+                return "C";
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IpCimek
+{
+    class Program
+    {
+        static void Main()
+        {
+            IP cim = new IP("192.168.11.10");
+            Console.WriteLine(cim.Osztaly());
+
+            Console.ReadKey();
+        }
+    }
+}
